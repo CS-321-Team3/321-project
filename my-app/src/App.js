50ffa80a3e70a86aa3a327c9f7867fe576e5476d
Toan Nguyen <toanttn2143work.com>
2025-03-07 22:24:54 -0500

    Simple front end UI

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import JobSpanner from './frontEnd'; // Import the JobSpanner component
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the JobSpanner component */}
+      <JobSpanner />
+    </div>
+  );
+}
+
+export default App;
