7a5856cac2059b1649946e95e0a2b1ec97ccf2be
Sophia Denayer <130000229+scdenayer@users.noreply.github.com>
2025-03-25 11:35:53 -0400

    fastapi_file.py
    basis for fastapi calls from sorter.py

---
+++ fastapi_file.py
@@ -0,0 +1,25 @@
+#Project: JobSpanner
+#Contributors: Sophia Denayer
+#Fast api file
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from sorter import potential_jobs
+from sorter import searched_words
+from sorter import resume_skills
+
+app = FastAPI()
+
+#sample 
+@app.get("/potential_jobs/", response_model = List[dict])
+def get_jobs():
+    return potential_jobs;
+    
+@app.get("/searched_words/", response_model = List[dict])
+def get_search():
+    return searched_words;
+
+@app.get("/resume_skills/", response_model = List[dict])
+def get_skills():
+    return resume_skills;
