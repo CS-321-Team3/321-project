dc543c801866f6cb22230c652aa759eab001ddda
Anoop Peterson <anoopdavid@outlook.com>
2025-04-03 07:57:53 -0400

    boilerplate code to run sqllite engine in fastapi environment

---
+++ my-app-auth/password-check.py
@@ -0,0 +1,42 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    secret_name: str
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    password: str = Field()
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session(): 
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+def lifespan(app: FastAPI):
+    create_db_and_tables()
+
+app = FastAPI(lifespan=lifespan)
